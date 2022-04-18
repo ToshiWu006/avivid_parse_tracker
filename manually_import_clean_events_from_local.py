e11a90f68ab0a7b567bf3d8500127caac82a109f
pine <ychanc2104@gmail.com>
2022-04-18 19:10:40 +0800

    TrackingParser.py(refactor), manually_import_clean_events_from_local.py(perf): directly import all web_id

--- manually_import_clean_events_from_local.py
+++ manually_import_clean_events_from_local.py
@@ -1,5 +1,5 @@
 from s3_parser import TrackingParser
-from import_tracker_clean_data_yesterday import save_six_clean_events, fetch_enable_analysis_web_id, main_import_tracker
+from import_tracker_data_byHour import save_six_clean_events
 import datetime, argparse
 
 
@@ -13,6 +13,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
     web_id = args.web_id
     date_start, date_end = args.date1_UTC8, args.date2_UTC8
+    print(f"import events form local, web_id: {web_id} and date from '{date_start}' to '{date_end}'")
 
     if date_start == None or date_end == None:
         ## default import last hour
@@ -22,12 +23,16 @@ if __name__ == "__main__":
         # tracking = TrackingParser(web_id, date_utc8, date_utc8)
 
     if web_id==None:
-        web_id_all = fetch_enable_analysis_web_id()
-        for web_id in web_id_all:
-            tracking = TrackingParser(web_id, date_start, date_end)
-            ## add six events df to instance
-            df_loaded, df_leaved, df_timeout, df_addCart, df_removeCart, df_purchased = tracking.get_six_events_df()
-            save_six_clean_events(df_loaded, df_leaved, df_timeout, df_addCart, df_removeCart, df_purchased)
+        tracking = TrackingParser(web_id, date_start, date_end)
+        df_loaded, df_leaved, df_timeout, df_addCart, df_removeCart, df_purchased = tracking.get_six_events_df_all(tracking.data_list)  # get_six_events_df_all
+        save_six_clean_events(df_loaded, df_leaved, df_timeout, df_addCart, df_removeCart, df_purchased)
+
+        # web_id_all = fetch_enable_analysis_web_id()
+        # for web_id in web_id_all:
+        #     tracking = TrackingParser(web_id, date_start, date_end)
+        #     ## add six events df to instance
+        #     df_loaded, df_leaved, df_timeout, df_addCart, df_removeCart, df_purchased = tracking.get_six_events_df() #get_six_events_df_all
+        #     save_six_clean_events(df_loaded, df_leaved, df_timeout, df_addCart, df_removeCart, df_purchased)
     else:
         tracking = TrackingParser(web_id, date_start, date_end)
         ## add six events df to instance
