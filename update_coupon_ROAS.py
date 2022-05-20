c85ae824571754de8478cedda1e5e9309d975411
pine <ychanc2104@gmail.com>
2022-05-20 19:42:10 +0800

    refactor: use DBhelper, feat: add fetch all activities

--- update_coupon_ROAS.py
+++ update_coupon_ROAS.py
@@ -1,7 +1,24 @@
-from db import MySqlHelper
+from db import DBhelper
 from basic import timing, logging_channels, to_datetime, curdate
 import pandas as pd
 from definitions import ROOT_DIR
+
+@timing
+def fetch_coupon_activity_all():
+    query = f"""
+    SELECT id, web_id, link_code, coupon_limit, coupon_type, coupon_amount, 
+    start_time, date_add(end_time,INTERVAL 1 DAY) as end_time, coupon_total
+    FROM addfan_activity WHERE web_id != 'rick'    
+    """
+    print(query)
+    data = DBhelper("rhea1-db0", is_ssh=True).ExecuteSelect(query)
+    columns = ['id', 'web_id', 'link_code', 'coupon_limit', 'coupon_type',
+               'coupon_amount', 'activity_start', 'activity_end', 'coupon_total']
+    coupon_price_limit = [int(d[3].split('limit-bill=')[1]) if d[3].find('limit-bill=')!=-1 else 0 for d in data]
+    df_coupon = pd.DataFrame(data, columns=columns).drop(columns=['coupon_limit'])
+    df_coupon['coupon_price_limit'] = coupon_price_limit
+    return df_coupon
+
 
 @timing
 def fetch_coupon_activity_running():
@@ -12,7 +29,7 @@ def fetch_coupon_activity_running():
     and web_id != 'rick'    
     """
     print(query)
-    data = MySqlHelper("rhea1-db0", is_ssh=True).ExecuteSelect(query)
+    data = DBhelper("rhea1-db0", is_ssh=True).ExecuteSelect(query)
     columns = ['id', 'web_id', 'link_code', 'coupon_limit', 'coupon_type',
                'coupon_amount', 'activity_start', 'activity_end', 'coupon_total']
     coupon_price_limit = [int(d[3].split('limit-bill=')[1]) if d[3].find('limit-bill=')!=-1 else 0 for d in data]
@@ -32,7 +49,7 @@ def fetch_coupon_activity_just_expired():
     and web_id != 'rick'
     """
     print(query)
-    data = MySqlHelper("rhea1-db0", is_ssh=True).ExecuteSelect(query)
+    data = DBhelper("rhea1-db0", is_ssh=True).ExecuteSelect(query)
     columns = ['id', 'web_id', 'link_code', 'coupon_limit', 'coupon_type',
                'coupon_amount', 'activity_start', 'activity_end', 'coupon_total']
     coupon_price_limit = [int(d[3].split('limit-bill=')[1]) if d[3].find('limit-bill=')!=-1 else 0 for d in data]
@@ -62,8 +79,8 @@ def fetch_n_coupon_sent_accept(coupon_id, activity_start, activity_end):
         AND date_time BETWEEN '{activity_start}' AND '{activity_end}'
     """
     print(query_accept)
-    data_sent = MySqlHelper("tracker").ExecuteSelect(query_sent)
-    data_accept = MySqlHelper("tracker").ExecuteSelect(query_accept)
+    data_sent = DBhelper("tracker").ExecuteSelect(query_sent)
+    data_accept = DBhelper("tracker").ExecuteSelect(query_accept)
     coupon_sent, coupon_accept = data_sent[0][0], data_accept[0][0]
     return coupon_sent, coupon_accept
 
@@ -72,7 +89,7 @@ def fetch_calc_types():
     SELECT web_id, type_total_price, type_cal_cost FROM web_push.cdp_tracking_settings where enable_analysis=1
     """
     print(query)
-    data = MySqlHelper("rheacache-db0", is_ssh=True).ExecuteSelect(query)
+    data = DBhelper("rheacache-db0", is_ssh=True).ExecuteSelect(query)
     type_total_price_dict, type_cal_cost_dict = {}, {}
     for d in data:
         type_total_price_dict.update({d[0]:d[1]})
@@ -182,7 +199,7 @@ def fetch_coupon_used_revenue_cost(web_id, coupon_id, coupon_type, coupon_cost, 
                         GROUP BY uuid , session_b) AS temp
                     """
     print(query)
-    data = MySqlHelper("tracker").ExecuteSelect(query)
+    data = DBhelper("tracker").ExecuteSelect(query)
     df_ROAS = pd.DataFrame(data, columns=['revenue', 'cost', 'coupon_used'])
     if coupon_type==3: # % off
         ## replace calculation of cost
@@ -201,9 +218,9 @@ def get_n_coupon_stat(df_ROAS, coupon_id, activity_start, activity_end, coupon_t
 
 def update_ROAS(df_ROAS, is_save=True):
     update_col = ['revenue', 'cost', 'coupon_sent', 'coupon_accept', 'coupon_used', 'avg_n_coupon']
-    query_update = MySqlHelper.generate_insertDup_SQLquery(df_ROAS, 'addfan_activity', update_col)
+    query_update = DBhelper.generate_insertDup_SQLquery(df_ROAS, 'addfan_activity', update_col)
     if is_save:
-        MySqlHelper("rhea1-db0", is_ssh=True).ExecuteUpdate(query_update, df_ROAS.to_dict('records'))
+        DBhelper("rhea1-db0", is_ssh=True).ExecuteUpdate(query_update, df_ROAS.to_dict('records'))
 
 @timing
 def fetch_coupon_cost(web_id, coupon_type, coupon_amount):
@@ -221,7 +238,7 @@ def fetch_coupon_cost(web_id, coupon_type, coupon_amount):
     if coupon_type==1:
         query = f"SELECT avg_shipping_price FROM addfan_stat WHERE web_id='{web_id}'"
         print(query)
-        coupon_cost = int(MySqlHelper("rhea1-db0", is_ssh=True).ExecuteSelect(query)[0][0])
+        coupon_cost = int(DBhelper("rhea1-db0", is_ssh=True).ExecuteSelect(query)[0][0])
     elif coupon_type==2:
         coupon_cost = int(coupon_amount)
     elif coupon_type==3:
@@ -280,3 +297,17 @@ if __name__ == "__main__":
         df_ROAS_expired_all = pd.concat([df_ROAS_expired_all,df_ROAS])
 
 
+    # ## force to update all existing coupon
+    # df_coupon_all = fetch_coupon_activity_all()
+    # df_coupon_all = df_coupon_all.query(f"web_id=='funselect'")
+    # df_ROAS_alls = pd.DataFrame()
+    # for i,row in df_coupon_all.iterrows():
+    #     coupon_id, web_id, link_code, coupon_type, coupon_amount, activity_start, activity_end, coupon_total, coupon_price_limit = row
+    #     type_total_price = type_total_price_dict[web_id] if web_id in type_total_price_dict.keys() else 0
+    #     type_cal_cost = type_cal_cost_dict[web_id] if web_id in type_cal_cost_dict.keys() else 0
+    #     df_ROAS_alls = main_update_addFan_ROAS(web_id, coupon_id, coupon_price_limit,
+    #                                               coupon_type, coupon_amount, coupon_total,
+    #                                               activity_start, activity_end,
+    #                                               type_total_price, type_cal_cost,
+    #                                               is_save=True)
+    #     df_ROAS_alls = pd.concat([df_ROAS_alls,df_ROAS])
